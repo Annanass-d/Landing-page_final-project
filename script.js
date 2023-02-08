81e2f4fbb66a175103e4a3945c57ce4c01325cbf
Anna_Dmitrieva <catdogcat000gmail.com>
2023-02-08 21:21:05 +0300

    added rating and input validation

---
+++ script.js
@@ -0,0 +1,52 @@
+const buttons = document.querySelectorAll('.rating_sign_input');
+
+buttons.forEach((radio) => {
+    radio.addEventListener('change', function (e) {
+        const checkedNumber = document.querySelector('.rating_sign_input:checked').value || undefined;
+
+        buttons.forEach((item) => {
+            const currentLabel = item.previousElementSibling.firstChild;
+            if (item.value <= checkedNumber) {
+                currentLabel.classList.add('check');
+            } else {
+                currentLabel.classList.remove('check');
+            }
+        });
+    });
+});
+
+
+const passwordEl = document.querySelector('#password');
+const passwordRetryEl = document.querySelector('#password-retry');
+const buttonFormEl = document.querySelector('.form__button');
+const inputEl = document.querySelectorAll('.contact__input');
+const formEl = document.querySelector('.contact__form');
+
+passwordRetryEl.addEventListener('input', function (e) {
+    if (passwordRetryEl.value === '') {
+        passwordEl.style.border = '1px solid #D9D9D9';
+        passwordRetryEl.style.border = '1px solid #D9D9D9';
+    } else if (passwordEl.value !== passwordRetryEl.value) {
+        passwordEl.style.border = '1px solid red';
+        passwordRetryEl.style.border = '1px solid red';
+    } else {
+        passwordEl.style.border = '1px solid green';
+        passwordRetryEl.style.border = '1px solid green';
+    }
+});
+
+formEl.addEventListener('submit', function (e) {
+    inputEl.forEach(element => {
+        if (element.value === '') {
+            e.preventDefault();
+            element.style.border = '1px solid red';
+        }
+    });
+
+    if (passwordEl.value !== passwordRetryEl.value) {
+        e.preventDefault();
+        passwordEl.style.border = '1px solid red';
+        passwordRetryEl.style.border = '1px solid red';
+    } 
+});
+
